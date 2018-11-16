46b8dac1d2e950c5c2043074a86a2098fa0ab15d
Sunny Patel <sunny2831@hotmail.co.uk>
2018-11-16 19:24:36 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,4 +1,17 @@
-
+DICTIONARY = {
+  "hello" => 'hi',
+  "to" => 2,
+  "two" => 2,
+  "too" => '2',
+  "for" => 4,
+  "For" => 4,
+  "four" => '4',
+  'be' => 'b',
+  'you' => 'u',
+  "at" => "@",
+  "and" => "&"
+}
+ 
  def word_substituter(tweet)
   tweet.split.map{|x| if DICTIONARY.has_key?(x) then DICTIONARY[x] else x end}.join(" ")
 end
