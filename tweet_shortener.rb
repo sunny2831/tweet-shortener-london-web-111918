26158a1d3ae6a5337d48f333d6f9a4e6188596bc
Sunny Patel <sunny2831@hotmail.co.uk>
2018-11-16 19:59:21 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -36,3 +36,11 @@ def selective_tweet_shortener(tweet)
     tweet
   end
 end
+
+def shortened_tweet_truncator
+  if word_substituter(tweet).length > 140
+    word_substituter(tweet)[0..137] + ("...")
+  else
+    word_substituter(tweet)
+  end
+end
