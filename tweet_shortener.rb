138093ff411cc1c6d8b989df587da33f89a4e6da
Sunny Patel <sunny2831@hotmail.co.uk>
2018-11-16 19:35:42 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,25 +1,26 @@
 def dictionary
   dictionary = {
-  "hello" => 'hi',
-  "to" => 2,
-  "two" => 2,
-  "too" => '2',
-  "for" => 4,
-  "For" => 4,
-  "four" => '4',
-  'be' => 'b',
-  'you' => 'u',
-  "at" => "@",
-  "and" => "&"
-}
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
 
- def word_substituter(tweet)
-  tweet_array = tweet.split.collect { |word|
-     if dictionary.keys.include?(word)
-       word = dictionary[word]
-     else
-        word
-     end
-   }
-   tweet_array.join(" ")
+
+def word_substituter(tweet)
+  tweetArray = tweet.split.collect do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word
+    end
+  end
+  tweetArray.join(" ")
 end
